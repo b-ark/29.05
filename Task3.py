85d463403fbd73a2e96b3fe8fdedbdaea220f755
Mihail Barkov <mvbarkov2@gmail.com>
2021-06-05 13:46:09 +0300

    main

--- Task3.py
+++ Task3.py
@@ -1,7 +1,12 @@
 # Use a list comprehension to make a list containing tuples (i, j)
 # where `i` goes from 1 to 10 and `j` is corresponding to `i` squared.
 
+# первый вариант, сделал его до того как проходил тест
 range_list = [x for x in range(1, 11)]
 squared_list = [x ** 2 for x in range(1, 11)]
 final_list = [(range_list[x], squared_list[x]) for x in range(0, 10)]
 print(final_list)
+
+# второй вариант, более компактный
+second_final_list = [(y, y ** 2) for y in range(1, 11)]
+print(second_final_list)
